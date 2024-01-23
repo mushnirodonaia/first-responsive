c92784a85ebe0ecbecfbd3688e47ce2a3d4df8ac
unknown <mushni011@gmail.com>
2024-01-23 19:38:20 +0400

    first responsive code

---
+++ java.java
@@ -0,0 +1,58 @@
+//function test (){
+    //console.log(1)
+    //console.log(2)
+  //  console.log(3)
+//}
+
+
+
+
+//for(let i = 0; i <= 5; i++){
+  //  test();
+//}
+
+
+function misalmeba(saxeli,asaki){
+    
+    let messege = alert('gamarjoba ${saxeli}, shen xar ${asaki}');
+}
+
+//let saxeli = prompt(enter name);
+//let asaki = prompt(enter age);
+
+//misalmeba(saxeli, asaki);
+
+//function mimateba(a,b){
+   // let plus =  a * b;
+  //  return plus
+//}
+
+//console.log( mimateba(2,4))
+
+// let globaluri = "me var globali"
+// function testFunction(){
+//     console.log(globaluri)
+// }
+
+// function gamravleba(a,b,c){
+//     return a*b*c 
+// }
+
+// let pasuxi = gamravleba(4,1,2)
+
+// console.log(pasuxi)
+
+
+// ARROW FUNCTION
+
+// let gamravleba = (x,y) => x * y
+
+// let pasuxi = gamravleba(3,5)
+
+
+// console.log(pasuxi) 
+
+
+
+
+
